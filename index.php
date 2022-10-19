4e5a4e40454141c7837eb46fa8e770e739bff2f3
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-10-19 08:51:04 -0300

    Criando form cadastro

--- index.php
+++ index.php
@@ -10,23 +10,41 @@
     
     <!-- menu -->
     <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand">Rede-Fotos</a>
-        <ul class="nav justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link" href="?home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?cadastro">Cadastro</a>
-            </li>
-        </ul>
-        <form class="d-flex form-inline" method="POST">
-            <input class="form-control mr-sm-2" name="txtLoginEmail" type="text" placeholder="E-mail">
-            <input class="form-control mr-sm-2" name="txtLoginSenha" type="password" placeholder="Senha">
-            <button class="btn btn-outline-success my-2 my-sm-0" name="btnLogin" type="submit">Login</button>
-        </form>
+        <div class="container">
+            <a class="navbar-brand">Rede-Facil</a>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="?home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?cadastro">Cadastro</a>
+                </li>
+            </ul>
+            <form class="d-flex form-inline" method="POST">
+                <input class="form-control mr-2" name="txtLoginEmail" type="text" placeholder="E-mail">
+                <input class="form-control mr-2" name="txtLoginSenha" type="password" placeholder="Senha">
+                <button class="btn btn-outline-success my-2 my-sm-0" name="btnLogin" type="submit">Login</button>
+            </form>
+
+        </div>
     </nav>
     <!-- menu -->
 
+    <div class="container">
+        <!-- rotas -->    
+            <?php
+                if(isset($_GET['cadastro'])){
+                    require_once 'view/cadastro.php';
+                }
+                elseif(isset($_GET['home'])){
+                    require_once './view/home.php';
+                }
+                else{
+                    require_once './view/home.php';
+                }
+            ?>
+        <!-- rotas -->
+    </div>
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
---
+++ view/cadastro.php
@@ -0,0 +1,44 @@
+<form>
+  <div class="mb-3">
+    <label class="form-label">E-mail</label>
+    <input 
+        type="email" 
+        class="form-control" 
+        name="txtEmail"  
+        placeholder="Seu melhor e-mail"
+    >
+  </div>
+
+  <div class="mb-3">
+    <label class="form-label">Password</label>
+    <input 
+        type="password" 
+        class="form-control"
+        name="txtSenha"
+    >
+  </div>
+
+  <div class="mb-3">
+    <label class="form-label">Nome</label>
+    <input 
+        type="text" 
+        class="form-control"
+        name="txtNome"
+        placeholder="Seu nome"
+    >
+  </div>
+
+  <div class="row mb-4">
+    <div class="col">
+        <label class="form-label">Foto perfil</label>
+        <input type="file" class="form-control" name="fotoPerfil">
+    </div>
+    <div class="col">
+        <label class="form-label">Foto capa</label>
+        <input type="file" class="form-control" name="fotoCapa">
+    </div>
+</div>
+  
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
