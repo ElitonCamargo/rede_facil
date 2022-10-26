38cd213e771940cd4e2d0a4b3ea6bfee298b1e5c
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-10-26 08:55:57 -0300

    Criando procedure usuario_consultarPorEmail

--- cmdSql.sql
+++ cmdSql.sql
@@ -1,1 +1,7 @@
 CREATE PROCEDURE `usuario_cadastrar`(IN `email` VARCHAR(100), IN `senha` VARCHAR(100), IN `nome` VARCHAR(100), IN `foto` VARCHAR(100), IN `capa` VARCHAR(100)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER INSERT INTO `usuario`(`email`, `senha`, `nome`, `foto`, `capa`) VALUES (email, senha, nome, foto, capa)
+
+CREATE PROCEDURE `usuario_consultarPorEmail`(IN `email` VARCHAR(100)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT * FROM usuario WHERE usuario.email = email
+
+-- git add .
+-- git reset --hard
+-- git pull
